f02efbcea9ec7859b2b829df793f8da373adc238
Stefan Chiriac <chiriacstefan@gmail.com>
2015-05-27 18:02:41 +0300

    admini theming

--- app/engine/AdminController.php
+++ app/engine/AdminController.php
@@ -19,10 +19,10 @@ abstract class AdminController extends Controller {
      * @return void
      */
     protected function initialize() {
-        parent::initialize();
-        if(!$this->request->isAjax()) {
+        if($this->request->isAjax()) {
             return;
         }
+    //    parent::initialize();
         $this->view->setMainView(THEMES_PATH . 'admin');
         $this->view->setLayout('admin');
         $this->_setupAssets();
@@ -32,26 +32,36 @@ abstract class AdminController extends Controller {
      * Setup assets
      * @return void
      */
-    protected function _setupAssets() {
+    private function _setupAssets() {
         $this->assets
-            ->collection('header-js')
-            //    ->setTargetPath(PUBLIC_PATH . 'assets/default/js/header.min.js')
-            //    ->setTargetUri('assets/default/js/header.min.js')
+            ->collection('header-js-min')
+            ->setTargetPath(PUBLIC_PATH . 'assets/mg_admin/js/header.min.js')
+            ->setTargetUri('assets/mg_admin/js/header.min.js')
             ->addJs('vendor/jquery/jquery-1.11.3.min.js')
             ->addJs('vendor/jquery/jquery-ui.min.js')
             ->addJs('vendor/bootstrap/js/bootstrap.min.js')
-            ->addJs('assets/default/js/mg.js');
-        //    ->join(true)
-        //    ->addFilter(new Phalcon\Assets\Filters\Jsmin());
+            ->addJs('assets/default/js/mg.js')
+            ->addJs('assets/mg_admin/js/app.min.js')
+            ->join(true)
+            ->addFilter(new \Phalcon\Assets\Filters\Jsmin());
+        $this->assets
+            ->collection('header-js');
+        $this->assets
+            ->collection('header-css-min')
+            ->setTargetPath(PUBLIC_PATH . 'assets/mg_admin/css/header.min.css')
+            ->setTargetUri('assets/mg_admin/css/header.min.css')
+            ->addCss('vendor/jquery/jquery-ui.min.css')
+            ->addCss('assets/mg_admin/css/AdminLTE.min.css')
+            ->addCss('assets/mg_admin/css/skins/skin-blue.min.css')
+            ->addCss('assets/mg_admin/css/style.css')
+            ->join(true)
+            ->addFilter(new \Phalcon\Assets\Filters\Cssmin());
         $this->assets
             ->collection('header-css')
-            //    ->setTargetPath(PUBLIC_PATH . 'assets/default/css/header.min.css')
-            //    ->setTargetUri('assets/default/css/header.min.css')
-            ->addCss('vendor/jquery/jquery-ui.min.css')
             ->addCss('vendor/bootstrap/css/bootstrap.min.css')
-            ->addCss('assets/default/css/style.css');
-        //    ->join(true)
-        //    ->addFilter(new Phalcon\Assets\Filters\Cssmin());
+            ->addCss('vendor/font-awesome/css/font-awesome.min.css');
     }
+
+
 
 }
--- app/engine/Controller.php
+++ app/engine/Controller.php
@@ -30,23 +30,72 @@ abstract class Controller extends \Phalcon\Mvc\Controller {
      */
     protected function _setupAssets() {
         $this->assets
-            ->collection('header-js')
-            //    ->setTargetPath(PUBLIC_PATH . 'assets/default/js/header.min.js')
-            //    ->setTargetUri('assets/default/js/header.min.js')
+            ->collection('header-js-min')
+            ->setTargetPath(PUBLIC_PATH . 'assets/default/js/header.min.js')
+            ->setTargetUri('assets/default/js/header.min.js')
             ->addJs('vendor/jquery/jquery-1.11.3.min.js')
             ->addJs('vendor/jquery/jquery-ui.min.js')
             ->addJs('vendor/bootstrap/js/bootstrap.min.js')
-            ->addJs('assets/default/js/mg.js');
-        //    ->join(true)
-        //    ->addFilter(new Phalcon\Assets\Filters\Jsmin());
+            ->addJs('assets/default/js/mg.js')
+            ->join(true)
+            ->addFilter(new \Phalcon\Assets\Filters\Jsmin());
+        $this->assets
+            ->collection('header-js');
+        $this->assets
+            ->collection('header-css-min')
+            ->setTargetPath(PUBLIC_PATH . 'assets/default/css/header.min.css')
+            ->setTargetUri('assets/default/css/header.min.css')
+            ->addCss('vendor/jquery/jquery-ui.min.css')
+            ->addCss('assets/default/css/style.css')
+            ->join(true)
+            ->addFilter(new \Phalcon\Assets\Filters\Cssmin());
         $this->assets
             ->collection('header-css')
-            //    ->setTargetPath(PUBLIC_PATH . 'assets/default/css/header.min.css')
-            //    ->setTargetUri('assets/default/css/header.min.css')
-            ->addCss('vendor/jquery/jquery-ui.min.css')
-            ->addCss('vendor/bootstrap/css/bootstrap.min.css')
-            ->addCss('assets/default/css/style.css');
-        //    ->join(true)
-        //    ->addFilter(new Phalcon\Assets\Filters\Cssmin());
+            ->addCss('vendor/bootstrap/css/bootstrap.min.css');
+    }
+
+    protected function _setupNavigation() {
+        return [
+            'admin' => [
+                'href' => 'admin',
+                'title' => 'Dashboard',
+                'prepend' => '<i class="glyphicon glyphicon-home"></i>'
+            ],
+            'users' => [
+                'title' => 'Manage',
+                'items' => [ // type - dropdown
+                    'admin/users' => [
+                        'title' => 'Users and Roles',
+                        'href' => 'admin/users',
+                        'prepend' => '<i class="glyphicon glyphicon-user"></i>'
+                    ],
+                    'admin/pages' => [
+                        'title' => 'Pages',
+                        'href' => 'admin/pages',
+                        'prepend' => '<i class="glyphicon glyphicon-list-alt"></i>'
+                    ],
+                    'admin/menus' => [
+                        'title' => 'Menus',
+                        'href' => 'admin/menus',
+                        'prepend' => '<i class="glyphicon glyphicon-th-list"></i>'
+                    ],
+                    'admin/languages' => [
+                        'title' => 'Languages',
+                        'href' => 'admin/languages',
+                        'prepend' => '<i class="glyphicon glyphicon-globe"></i>'
+                    ],
+                    'admin/files' => [
+                        'title' => 'Files',
+                        'href' => 'admin/files',
+                        'prepend' => '<i class="glyphicon glyphicon-file"></i>'
+                    ],
+                    'admin/packages' => [
+                        'title' => 'Packages',
+                        'href' => 'admin/packages',
+                        'prepend' => '<i class="glyphicon glyphicon-th"></i>'
+                    ]
+                ]
+            ],
+        ];
     }
 }
