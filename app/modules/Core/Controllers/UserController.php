cd44b0c4efdb0ce97365973c46689f9c6c0a1247
anigit <chiriacstefan@gmail.com>
2015-06-02 22:53:48 +0300

    login user

--- app/modules/Core/Controllers/UserController.php
+++ app/modules/Core/Controllers/UserController.php
@@ -25,8 +25,26 @@ class UserController extends Controller {
     public function loginAction() {
         $this->_setTitle('Login');
         $form = new LoginForm();
-        $username = $this->request->getPost('username', 'alphanum');
-        $password = $this->request->getPost('password');
+        if ($this->request->isPost()) {
+            if ($this->security->checkToken()) {
+            //    $post = $this->request->getPost('username', 'alphanum');
+            //    if ($form->isValid()) {
+                    $username = $this->request->getPost('username', 'alphanum');
+                    $password = $this->request->getPost('password');
+                    $user = User::findFirst("username='$username'");
+                    if($username) {
+                        if ($this->security->checkHash($password, $user->password)) {
+                            $this->registerSession($user);
+                            $this->response->redirect('user/login');
+                        } else {
+                            $this->flash->error('Username or password is invalid');
+                        }
+                    }
+            //    } else {
+           //         $this->_flashErrors($form);
+           //     }
+            }
+        }
         $this->view->form = $form;
     }
 
@@ -37,8 +55,6 @@ class UserController extends Controller {
         $this->_setTitle('Register');
         $form = new RegisterForm();
         if ($this->request->isPost()) {
-        //    $name = $this->request->getPost('name', array('string', 'striptags'));
-            /*
             $username = $this->request->getPost('username', 'alphanum');
             $email = $this->request->getPost('email', 'email');
             $password = $this->request->getPost('password');
@@ -60,22 +76,6 @@ class UserController extends Controller {
                 $form->clear();
                 $this->flash->success('Thanks for signing up, please log-in to manage your account');
             }
-            */
-            $post = $this->request->getPost();
-            $user = new User();
-            $user->setRole(1);
-            $user->setStatus(0);
-            $form->bind($post, $user);
-            if ($form->isValid()) {
-                if ($user->save()) {
-                    $form->clear();
-                    $this->flash->success('Thanks for signing up, please log-in to manage your account');
-                } else {
-                    $this->_flashErrors($user);
-                }
-            } else {
-                $this->_flashErrors($form);
-            }
         }
         $this->view->form = $form;
     }
@@ -84,11 +84,115 @@ class UserController extends Controller {
      * Register an authenticated user into session data
      * @param User $user
      */
-    private function _registerSession(User $user)
-    {
+    private function registerSession(User $user) {
         $this->session->set('auth', array(
             'id' => $user->getId(),
             'username' => $user->getUsername()
         ));
     }
+
+    public function logoutAction() {
+        if ($this->request->isPost()) {
+            if ($this->security->checkToken()) {
+                $this->session->remove('auth');
+                $this->response->redirect('');
+                return $this->response->send();
+            } else {
+                die("Security errors");
+            }
+        } else {
+            die("Security errors");
+        }
+    }
+
+    /**
+     * Confirms an e-mail, if the user must change their password then changes it
+     */
+    /*
+    public function confirmEmailAction()
+    {
+        $code = $this->dispatcher->getParam('code');
+        $confirmation = EmailConfirmations::findFirstByCode($code);
+        if (!$confirmation) {
+            return $this->dispatcher->forward(array(
+                'controller' => 'index',
+                'action' => 'index'
+            ));
+        }
+        if ($confirmation->confirmed != 'N') {
+            return $this->dispatcher->forward(array(
+                'controller' => 'session',
+                'action' => 'login'
+            ));
+        }
+        $confirmation->confirmed = 'Y';
+        $confirmation->user->active = 'Y';
+
+        // Change the confirmation to 'confirmed' and update the user to 'active'
+        if (!$confirmation->save()) {
+            foreach ($confirmation->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+            return $this->dispatcher->forward(array(
+                'controller' => 'index',
+                'action' => 'index'
+            ));
+        }
+
+        // Identify the user in the application
+        $this->auth->authUserById($confirmation->user->id);
+
+        // Check if the user must change his/her password
+        if ($confirmation->user->mustChangePassword == 'Y') {
+            $this->flash->success('The email was successfully confirmed. Now you must change your password');
+            return $this->dispatcher->forward(array(
+                'controller' => 'users',
+                'action' => 'changePassword'
+            ));
+        }
+        $this->flash->success('The email was successfully confirmed');
+        return $this->dispatcher->forward(array(
+            'controller' => 'users',
+            'action' => 'index'
+        ));
+    }
+
+    public function resetPasswordAction()
+    {
+        $code = $this->dispatcher->getParam('code');
+        $resetPassword = ResetPasswords::findFirstByCode($code);
+        if (!$resetPassword) {
+            return $this->dispatcher->forward(array(
+                'controller' => 'index',
+                'action' => 'index'
+            ));
+        }
+        if ($resetPassword->reset != 'N') {
+            return $this->dispatcher->forward(array(
+                'controller' => 'session',
+                'action' => 'login'
+            ));
+        }
+        $resetPassword->reset = 'Y';
+
+        // Change the confirmation to 'reset'
+        if (!$resetPassword->save()) {
+            foreach ($resetPassword->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+            return $this->dispatcher->forward(array(
+                'controller' => 'index',
+                'action' => 'index'
+            ));
+        }
+
+        // Identify the user in the application
+        $this->auth->authUserById($resetPassword->usersId);
+        $this->flash->success('Please reset your password');
+        return $this->dispatcher->forward(array(
+            'controller' => 'users',
+            'action' => 'changePassword'
+        ));
+    }
+    */
 }
